e5a85a24f55d044f5381d5c1f9e383f941f8f1aa
ElenaPetrus <79158516+ElenaPetrus@users.noreply.github.com>
2022-12-25 18:24:13 +0100

    Update task_one.py

--- task_one.py
+++ task_one.py
@@ -1,3 +1,11 @@
 # 'hello world'
-a = 'hello world'
-print(a)
+# a = 'hello world'
+#  print(a)
+
+# a = 'hello world'
+# print(a*5, sep='')
+
+a = '#########'
+b = '#\t#'
+print(a, b, b, b, a, sep='\n', end='\n\n\n')
+print(b, b, a, b, b, sep='\n')
