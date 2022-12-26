694bdb3bc9666162fffbf8971a0e861e877b6a7a
ElenaPetrus <79158516+ElenaPetrus@users.noreply.github.com>
2022-12-26 12:21:51 +0100

    add option4

--- task_123_221221.py
+++ task_123_221221.py
@@ -17,8 +17,12 @@ day = str(date.today())
 # print(want_to_print)
 
 # option 3
-want_to_print = 'Good day {}! {} is a perfect day to learn some python.'
-print(want_to_print.format(full_name, day))
+# want_to_print = 'Good day {}! {} is a perfect day to learn some python.'
+# print(want_to_print.format(full_name, day))
+
+# option 4
+want_to_print = 'Good day {1}! {0} is a perfect day to learn some python.'
+print(want_to_print.format(day, full_name))
 
 
 a = 12
