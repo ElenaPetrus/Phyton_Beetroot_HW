05f8625e8055d6ff3fabdd965254fe417cf80582
ElenaPetrus <79158516+ElenaPetrus@users.noreply.github.com>
2023-01-15 21:09:28 +0100

    HM  241222

---
+++ task_123_221224.py
@@ -0,0 +1,29 @@
+# Task 1
+# user_string = input('Please write the sentance:')
+# if len(user_string) < 2:
+#     print('please write the sentance, that have two letters at least')
+# else:
+#     print(user_string[:2]+user_string[-2:])
+
+
+# Task 2
+# while True:
+#     if len(user_phone_string) < 10:
+#         print('your phone number should contain 10 digits. Please try again.')
+#     elif user_phone_string.isdigit() == False:
+#         print('You phone number does not contains all numbers.Please try again.')
+#     else:
+#         print(f'You phone number {user_phone_string} has been saved.')
+#         break
+
+
+# Task 3
+# stored_name = 'olena'
+# while True:
+#     user_name = input('Please write you name:').lower()
+
+#     if user_name != stored_name:
+#         print('Are you sure that it is your name?')
+#     else:
+#         print('You have got access to your bak safe box')
+#         break
